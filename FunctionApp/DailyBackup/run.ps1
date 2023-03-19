7e1f9cf833730f4fb61b0ad3465c810327eb22f3
Bernhard <bernhard.fluer@axians.at>
2023-03-19 16:45:18 +0100

    changed strategy to single share with multi folder

--- FunctionApp/DailyBackup/run.ps1
+++ FunctionApp/DailyBackup/run.ps1
@@ -32,10 +32,6 @@ if ($Timer) {
     Import-Module Az.Storage
     Import-Module AzTable
 
-    #Authorizize with Azure
-    Write-Debug "Connect to Azure Account with managed identity"
-    Connect-AzAccount -Identity
-
     # Get the Storage Account Context
     try {
         #Get Context
@@ -49,25 +45,25 @@ if ($Timer) {
         Write-Error -Message "Failed to get source storage account. Check if permissions are correct: $_"
     }
 
+    #Check if File Share exists
+    $TableFileShare = Get-AzStorageShare -Name "backup$StorageAccountNameSource" -Context $FileShareSAContext -ErrorAction SilentlyContinue
+        
+    if (!$TableFileShare) {
+        Write-Debug "Creating Backup-FileShare..."
+        #Create File Share
+        New-AzRmStorageShare -ResourceGroupName $RGNameTarget -StorageAccountName $StorageAccountNameTarget -Name "backup$StorageAccountNameSource" -AccessTier Cool
+    } else {
+        Write-Debug "Backup-FileShare already exists! Continue..."
+    }
+
     foreach ($Table in $Tables)
     {
-        $TableName = $Table.Name
-
-        #Check if File Share exists
-        $TableFileShare = Get-AzStorageShare -Name $TableName.ToLower() -Context $FileShareSAContext
-        
-        if (!$TableFileShare) {
-            Write-Debug "Creating Backup-FileShare $TableFileShare ..."
-            #Create File Share
-            New-AzRmStorageShare -ResourceGroupName $RGNameTarget -StorageAccountName $StorageAccountNameTarget -Name $TableName.ToLower() -AccessTier Cool
-        } else {
-            Write-Debug "Backup-FileShare $TableFileShare already exists! Continue..."
-        }
+        $TableName = $Table.Name    
 
         # Create Folder for Table if not exists
         Write-Debug "Check if folder $TableName exists..."
 
-        $targetFolder = Get-Item -Path $env:TEMP/$TableName -ErrorAction Ignore
+        $targetFolder = Get-Item -Path $env:TEMP/$TableName -ErrorAction SilentlyContinue
         if(!$targetFolder) {
             Write-Debug "Creating folder $TableName ..."
             $targetFolder = New-Item -Name $TableName -ItemType Directory -Path $env:TEMP
@@ -83,7 +79,7 @@ if ($Timer) {
         foreach ($Partition in $Partitions)
         {
             #Get the data for the partition
-            $PartitionData = Get-AzTableRow -Table $CloudTable -PartitionKey $Partition -ErrorAction Ignore
+            $PartitionData = Get-AzTableRow -Table $CloudTable -PartitionKey $Partition -ErrorAction SilentlyContinue
             #Convert the data to JSON and write to a file in temp folder
             $PartitionData | ConvertTo-Json | Out-File -FilePath "$env:TEMP\$TableName\$($Partition).json"
         }
@@ -92,11 +88,33 @@ if ($Timer) {
     Write-Debug "Starting upload to backup account"
     foreach ($table in $Tables) {
         $tableName = $table.Name
+        # Create Folder for Table on File Share if not exists
+        $folder = Get-AzStorageFile -ShareName "backup$StorageAccountNameSource" -Context $FileShareSAContext -ErrorAction SilentlyContinue | Where-Object {$_.Name -eq $tableName.ToLower()}
+        if (!$folder) {
+            New-AzStorageDirectory -ShareName "backup$StorageAccountNameSource" -Context $FileShareSAContext -Path $tableName.ToLower()
+        } else {
+            Write-Debug "Folder $tableName exists on File Share! Continue ..."
+        }
+    }
+    
+    # Remove old folders
+    $share = Get-AzStorageFile -ShareName "backup$StorageAccountNameSource" -Context $FileShareSAContext | Where-Object {$_.Type -eq "Directory"}
+    foreach ($dir in $share) {
+        if ($Tables -contains $dir.Name) {
+            continue
+        } else {
+            Write-Debug "Remove old folder $dir.Name"
+        }
+        $dir | Remove-AzStorageFile -Force
+    }
+
+    foreach ($table in $Tables) {
+        $tableName = $table.Name
         # Upload to file share
         $CurrentFolder = (Get-Item $env:TEMP/$tableName).FullName
-        $Container = Get-AzStorageShare -Name $tableName.ToLower() -Context $FileShareSAContext
+        $Container = Get-AzStorageShare -Name "backup$StorageAccountNameSource" -Context $FileShareSAContext
         Get-ChildItem -Path $env:TEMP/$tableName -Recurse | Where-Object { $_.GetType().Name -eq "FileInfo"} | ForEach-Object {
-            $path=$_.FullName.Substring($CurrentFolder.Length+1).Replace("\","/")
+            $path="$tableName/$($_.FullName.Substring($CurrentFolder.Length+1).Replace("\","/"))"
             Write-Output "Upload Partition $($_.FullName) from table $tableName"
             Set-AzStorageFileContent -Sharename $Container.Name -Context $FileShareSAContext -Source $_.FullName -Path $path -Force
         }
